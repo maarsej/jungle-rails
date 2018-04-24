19a4e93ae1bd9520a491963c7773beaae9c7b3fa
Nucleartom <jacob.maarse@gmail.com>
2018-04-24 21:00:04 +0000

    Setup complete starting first feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-
-  root to: 'products#index'
+  root to: "products#index"
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    put    :add_item
+    put :add_item
     delete :remove_item
   end
 
   resources :orders, only: [:create, :show]
 
   namespace :admin do
-    root to: 'dashboard#show'
+    root to: "dashboard#show"
     resources :products, except: [:edit, :update, :show]
   end
 
