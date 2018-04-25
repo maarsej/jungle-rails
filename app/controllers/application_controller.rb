9a2dfcc14a003f89a0b5a1bab93e1dadbc66e6bb
Nucleartom <jacob.maarse@gmail.com>
2018-04-25 23:59:06 +0000

    Added basic http authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] || nil
   end
 
   helper_method :current_user
